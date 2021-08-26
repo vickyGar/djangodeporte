23e1e3defb195fa1753cbb948bd8e121b703b52e
Vicky <gcardonaalba@gmail.com>
2021-08-25 18:10:34 -0600

    proyecto

--- DevDeporte/settings.py
+++ DevDeporte/settings.py
@@ -70,18 +70,31 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'DevDeporte.wsgi.application'
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DBdeportivo',
+        'USER': 'Vicky',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': 5432
+    }
+ }
 
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-""""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 """
 
 import dj_database_url
@@ -92,7 +105,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
--- apps/Deportes/models.py
+++ apps/Deportes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class garment(models.Model):
+    pk_id_garment = models.AutoField(primary_key=True, null=False, blank=False)
+    mark = models.CharField(max_length=50, null=False, blank=False)
+    size = models.DateField(auto_now=False, auto_now_add=True, null=False, blank=False)
+    shop = models.IntegerField(null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    price = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=2)
+    discount = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=2)
+    amount = models.IntegerField(null=False, blank=False)
+
+class mark(models.Model):
+    pk_id_mark = models.AutoField(primary_key=True, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
