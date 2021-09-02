fc81d818b43178c688bf53710348278785194213
Vicky <gcardonaalba@gmail.com>
2021-09-01 18:26:40 -0600

    change templates and static

--- apps/Deportes/urls.py
+++ apps/Deportes/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact')
 ]
--- apps/Deportes/views.py
+++ apps/Deportes/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def contact(request):
+    return render(request, 'contact.html')
+
 
